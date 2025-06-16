488059c6625b4d542b1f2f665351923670c09ef2
DrewBrandt <DrewBrandt@users.noreply.github.com>
2025-06-16 18:51:55 +0000

    deploy: 5bc7ac8db6d3631f7a7e4b26cbd2a3c68f3375fe

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -30,7 +30,7 @@ const RESOURCES = {"canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"flutter_bootstrap.js": "bc37bc82616b8dc1aefe36c6302a3ec7",
+"flutter_bootstrap.js": "4db46371824271c1d7cb04a903a81aca",
 "version.json": "bd1e463857aae365ee44fc2c376fdd3a"};
 // The application shell files that are downloaded before a service worker can
 // start.
